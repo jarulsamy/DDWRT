79595f7183fd68857689b1bc4e656120a0f7f222
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2020-06-21 12:29:54 -0600

    :art: Reformatted to fit standards.

--- gpio_test.sh
+++ gpio_test.sh
@@ -2,10 +2,10 @@
 i=13
 end=30
 while [ $i -le $end ]; do
-        echo $i
-        gpio disable $i
-        sleep 2
-        gpio enable $i
-        sleep 2
-            i=$(($i+1))
-        done
+    echo $i
+    gpio disable $i
+    sleep 2
+    gpio enable $i
+    sleep 2
+    i=$(($i + 1))
+done
